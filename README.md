68ea9069f24ddfcb247c20e9cab27594cc42c5ec
anspuli <ans.bob@gmail.com>
2014-08-17 11:43:00 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+  python  module to query range server
+
+  Example usage 
+
+  #!/usr/bin/python
+  from range import expand
+  print expand("range hostname",range_port,"@range_query")
---
+++ range.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#  python  module to query range server
+#  Example usage 
+#  #!/usr/bin/python
+#  from range import expand
+#  print expand("range hostname",9999,"@range_query")
+
+
+import os,sys,socket
+import requests
+
+def expand(host,port,range_query):
+  rport = 9999   # range port is set to 9999 by default
+  if host is not None:
+    rhost = host
+  elif resolv_hostname("range") is not None:
+    rhost = "range"
+  else:
+    rhost = get_environ_host()
+
+  if port is not None:
+    rport = port
+  else:
+    rport = get_environ_port()
+
+  if range_query is not None:
+    rquery = range_query
+  else:  
+    print "Range query is empty..exiting.."
+    sys.exit()
+
+  uri = 'http://%s:%d/range/list?%s' % (host, port, rquery)
+  try:
+    result = requests.get(uri)
+  except:
+    print "A error occurred while quering..exiting"
+    print e
+    sys.exit()
+  return result.content 
+#  print sorted(result.content.splitlines()) 
+
+def get_environ_host():
+  if os.getenv('RANGE_HOST') is not None:
+    range_host = os.getenv('RANGE_HOST')
+    return range_host
+  else:
+    print "range host is undefined.. please set RANGE_HOST env variable or pass range host to the module"
+    sys.exit() 
+
+def get_environ_port():
+  if os.getenv('RANGE_PORT') is not None:
+    range_port = os.getenv('RANGE_PORT')
+    return range_port
+  else:
+    print "range port is undefined.. please set RANGE_PORT env variable or pass range port to the module (default 9999)"
+    sys.exit() 
+
+def resolv_hostname(hostname):
+  try:
+    socket.gethostbyname(hostname)
+    return 1
+  except socket.error:
+    return None
